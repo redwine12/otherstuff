fce888b65e0ef5cee0bdd226713c083dfa3d2aa5
redwine12 <migonyoung01@gmail.com>
2023-02-02 12:02:58 +0900

    MVVM Convert

---
+++ MVVM/Combine-MVVM/AppleFramework/FrameworkDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FrameworkDetailViewModel.swift
+//  AppleFramework
+//
+//  Created by 이재영 on 2023/02/02.
+//
+
+import Foundation
---
+++ MVVM/Combine-MVVM/AppleFramework/FrameworkListViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  FrameworkListViewModel.swift
+//  AppleFramework
+//
+//  Created by 이재영 on 2023/02/02.
+//
+
+import Foundation
+import Combine
+
+final class FrameworkListViewModel {
+    
+    init(items: [AppleFramework], selectedItem: AppleFramework? = nil) {
+        self.items = CurrentValueSubject(items)
+        self.selectedItem = CurrentValueSubject(selectedItem)
+    }
+    
+    // Data => Output
+    var items = CurrentValueSubject<[AppleFramework], Never>(AppleFramework.list)
+    let selectedItem: CurrentValueSubject<AppleFramework?, Never>
+    
+    // User Action => Input
+    let didSelect = PassthroughSubject<AppleFramework, Never>()
+    
+    func didSelect(at indexPath: IndexPath) {
+        selectedItem.send(items)
+    }
+}
